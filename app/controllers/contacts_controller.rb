1d5f629421a80488bc63fa320e37ed62c0524660
Rails test <rb.ricardo01@gmail.com>
2016-09-23 12:19:29 -0300

    Added validations and create action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,24 @@
 class ContactsController < ApplicationController
+
+
 	def new
 		@contact = Contact.new
 	end
 	
 	def create
-		
+		@contact = Contact.new(contact_params)
+
+		if @contact.save
+			flash[:success] = "Message sent."
+			redirect_to new_contact_path
+		else
+			flash[:danger] = "Error accured, msg not be sent!"
+			redirect_to new_contact_path
+		end
+	end
+
+	private
+	def contact_params
+		params.require(:contact).permit(:name, :email, :comments)
 	end
 end
