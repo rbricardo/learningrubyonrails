5da94122334a3c4669b9e6014569f70aba512485
Rails test <rb.ricardo01@gmail.com>
2016-10-05 09:18:25 -0300

    Modified devise for stripe

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,37 +1,38 @@
 $(document).ready(function() {
-	Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
-	//Watch for a form submission:
+  Stripe.setPublishableKey($('meta[name="stripe-key"]').attr('content'));
+  // Watch for a form submission:
+  $("#form-submit-btn").click(function(event) {
+    event.preventDefault();
+    $('input[type=submit]').prop('disabled', true);
+    var error = false;
+    var ccNum = $('#card_number').val(),
+        cvcNum = $('#card_code').val(),
+        expMonth = $('#card_month').val(),
+        expYear = $('#card_year').val();
 
-	$("#form-submit-btn").click(function(event) {
-		event.preventDefault();
-		$('input(type-submit)').prop('disabled', true);
-		var error = false;
-		var ccNum = $('#card_number').val(),
-			cvNum = $('#card_code').val(),
-			expMonth = $('#card_month').val(),
-			expYear = $('#card_year').val();
+    if (!error) {
+      // Get the Stripe token:
+      Stripe.createToken({
+        number: ccNum,
+        cvc: cvcNum,
+        exp_month: expMonth,
+        exp_year: expYear
+      }, stripeResponseHandler);
+    }
+    return false;
+  }); // form submission
+  
+  function stripeResponseHandler(status, response) {
+    // Get a reference to the form:
+    var f = $("#new_user");
 
-		if (!error) {
+    // Get the token from the response:
+    var token = response.id;
 
-			Stripe.createToken({
-				number: ccNum,
-				cvc: cvNum,
-				exp_month: expMonth,
-				exp_year: expYear
-			}, stripeResponseHandler);
-		}
-		return false;
-	});
+    // Add the token to the form:
+    f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
 
-	function stripeResponseHandler(status, response) {
-		var f = $("#new_user");
-
-		var token = response.id;
-
-
-		f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token +'" />');
-
-		f.get(0).submit();
-	}
-
+    // Submit the form:
+    f.get(0).submit(); 
+  }
 });
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :configure_premitted_paramaters, if: :devise_controller?
+
+  protected
+  def  configure_premitted_paramaters
+  	devise_paramater_sanitizer.for(:sign_up) {|u| u.permit(:name, :stripe_card_token, 
+  		:email, :password, :password_confirmation)}  	
+  end
 end
